f9d1928a7cc9091351109fc1248bd2217f4e0d7a
Matthew Egan <matthewj.egan@hotmail.com>
2019-09-19 15:46:27 +1000

    pass raw body to validate_webhook

--- drf_model_pusher/authentication.py
+++ drf_model_pusher/authentication.py
@@ -1,11 +1,12 @@
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.request import Request
 
 from drf_model_pusher.providers import PusherProvider
 
 
 class PusherWebhookAuthentication(BaseAuthentication):
-    def authenticate(self, request):
+    def authenticate(self, request: Request):
         """
         Makes sure to validate auth headers with Pusher
         https://pusher.com/docs/channels/server_api/webhooks#authentication
@@ -15,7 +16,7 @@ class PusherWebhookAuthentication(BaseAuthentication):
         validated_data = PusherProvider().client.validate_webhook(
             key=request.META.get("HTTP_X_PUSHER_KEY"),
             signature=request.META.get("HTTP_X_PUSHER_SIGNATURE"),
-            body=request.data
+            body=request._request.body
         )
 
         if validated_data is None:
