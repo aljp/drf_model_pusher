ea9bffc5e1b07258abda76c8347cafd81f5ba082
Russell Martin <russell@divipay.io>
2021-11-26 09:33:56 +1100

    Add channel name prefix

--- drf_model_pusher/providers.py
+++ drf_model_pusher/providers.py
@@ -71,8 +71,12 @@ class PusherProvider(object):
         :return:
         """
         response = self.client.channels_info()
-        occupied_channels = response.get("channels", {}).keys()
-
+        occupied_channels = list(
+            map(
+                lambda occupied_channel: "drf-model-pusher:occupied:{}".format(occupied_channel),
+                response.get("channels", {}).keys()
+            )
+        )
         cache.set_many(
             dict.fromkeys(occupied_channels, True)
         )
