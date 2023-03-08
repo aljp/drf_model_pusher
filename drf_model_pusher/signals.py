e5d8888e2dee66517e04c56b4f50d998ee6da834
Russell Martin <russell@letsweel.com>
2023-03-08 21:11:48 +1100

    improvement: django 4 support for Signal

--- drf_model_pusher/signals.py
+++ drf_model_pusher/signals.py
@@ -1,10 +1,6 @@
 from django.dispatch import Signal
 
 
-view_post_save = Signal(
-    providing_args=["instance", "channels", "event_name", "data", "socket_id"]
-)
+view_post_save = Signal()
 
-view_pre_destroy = Signal(
-    providing_args=["instance", "channels", "event_name", "data", "socket_id"]
-)
+view_pre_destroy = Signal()
